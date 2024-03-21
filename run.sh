b0ac35dff9581a78b97f3e31673c6dabe900e307
wushono <152370247+wushono@users.noreply.github.com>
2024-03-21 21:22:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-0dd5b434-27b0-450a-a21e-b61a63a25f89//)
 
 
 echo "$curl_output" >> Update.tsx
